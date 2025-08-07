988e56f02e99461ee29729e616427053fa0a6592
no-wuttichai <no.wuttichai@gmail.com>
2025-08-07 16:19:24 +0700

    Update meta.blade.php

--- resources/views/partials/meta.blade.php
+++ resources/views/partials/meta.blade.php
@@ -1,3 +1,4 @@
+<link rel="icon" type="image/x-icon" href="images/logo.jpg">
 <meta name="google-site-verification" content="bgYGmkSluS5HbViQrx0FP2UKCt3WGWrLwPparMnxzvU">
 <meta name="Description" content="ไฟหน้าโปรเจคเตอร์ ไฟหน้ารถยนต์ หลอดไฟซีนอน บัลลาสไฟซีนอน ไฟเดย์ไลท์ หลอดไฟแอลอีดีรถยนต์ ไฟหน้ารถยนต์ รับอัพเกรด ซ่อมแซม ระบบไฟรถยนต์ทุกรุ่น สว่าง ปลอดภัย ไม่แยงตาเพื่อนร่วมทาง หล่อครบจบที่นี่ BT Premium auto xenon สาขา สุขสวัสดิ์ คุณภาพสูง ราคาไม่แพง">
 <meta property="og:title" content="BT Premium Auto Xeon - สาขาสุขสวัสดิ์">
